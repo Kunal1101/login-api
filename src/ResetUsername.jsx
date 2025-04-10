f09c39c0c44af7a2b77c1eae1f3cb1ff8189736f
Kunal Mishra <kunalmishra8127@gmail.com>
2025-04-10 15:20:16 +0530

    edit username form

---
+++ src/ResetUsername.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import "./App.css";
+import { useNavigate } from "react-router-dom";
+
+function ResetUsername() {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Token sent:", token);
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/update-username`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ username }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      console.log("Token sent:", token);
+    } catch (error) {
+      console.error("Error:", error.message);
+      alert("Update failed. Please check your credentials or try again later.");
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded-lg shadow-lg w-80"
+      >
+        <h2 className="text-xl font-bold mb-4 text-center">Reset Username</h2>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <span className="cursor-pointer" onClick={() => navigate("/")}>
+          Back
+        </span>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+        >
+          Reset Username
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ResetUsername;
--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,15 @@ import {
 } from "react-router-dom";
 import ResetPassword from "./ResetPassword.jsx";
 import Login from "./Login.jsx";
+import EditUsername from "./ResetUsername.jsx";
+import ResetUsername from "./ResetUsername.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route path="" element={<Login />} />
       <Route path="forgot-password" element={<ResetPassword />} />
+      <Route path="reset-username" element={<ResetUsername />} />
     </Route>
   )
 );
